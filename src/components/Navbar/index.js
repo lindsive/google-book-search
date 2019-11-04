f86de88cc0a9296676ea119336a7cf9f7f2f1381
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-11-03 20:07:07 -0500

    created and rendered Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return(
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="/">Google Book Search</a>
+    </nav>
+    );
+}
+
+export default Navbar;
